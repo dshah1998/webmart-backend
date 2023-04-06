60d2fc442ae062c31892b90e3655f9cb5c97f44e
sarangp3010 <spatel25@albany.edu>
2023-04-06 14:19:41 -0400

    remove content

--- src/controller/userCards.ts
+++ src/controller/userCards.ts
@@ -5,29 +5,6 @@ import StripeCardsService from '../service/StripeCards';
 
 import { Actions } from '../constants';
 
-export const getAllValidation = {
-  query: Joi.object({
-    limit: Joi.number().max(100).allow(null).default(3),
-    object: Joi.string().allow(null).default('card'),
-  }),
-};
-export const getAll = () => async (req: Request, res: Response): Promise<void> => {
-  const {
-    user,
-    query: { limit, object },
-  } = req;
-
-  const service = new StripeCardsService();
-  const result = await service.execute({
-    limit: Number(limit),
-    object: object?.toString() || 'card',
-    stripeCustomerId: user?.stripeCustomerId || '',
-    action: Actions.LIST,
-  });
-
-  res.status(200).json({ cards: result?.sources?.data });
-};
-
 export const createUsersCardValidation = {
   body: Joi.object({
     cardNumber: Joi.string().max(25).required(),
@@ -54,73 +31,6 @@ export const createUsersCard = () => async (req: Request, res: Response): Promis
     userId: user?.id,
     action: Actions.CREATE,
     stripeCustomerId: user?.stripeCustomerId || '',
-  });
-
-  res.status(200).json(result);
-};
-
-export const updateUsersCardValidation = {
-  body: Joi.object({
-    expMonth: Joi.string().required(),
-    expYear: Joi.string().required(),
-    name: Joi.string().required(),
-  }),
-  params: Joi.object({ id: Joi.string().required() }),
-};
-export const updateUsersCard = () => async (req: Request, res: Response): Promise<void> => {
-  const {
-    user,
-    body: { expMonth, expYear, name },
-    params: { id },
-  } = req;
-
-  const service = new StripeCardsService();
-  const result = await service.execute({
-    name,
-    expYear,
-    expMonth,
-    stripeCardId: id,
-    userId: user?.id,
-    action: Actions.UPDATE,
-    stripeCustomerId: user?.stripeCustomerId || '',
-  });
-
-  res.status(200).json(result);
-};
-
-export const deleteUsersCardValidation = {
-  params: Joi.object({ id: Joi.string().required() }),
-};
-export const deleteUsersCard = () => async (req: Request, res: Response): Promise<void> => {
-  const {
-    user,
-    params: { id },
-  } = req;
-
-  const service = new StripeCardsService();
-  const result = await service.execute({
-    stripeCardId: id,
-    action: Actions.DELETE,
-    stripeCustomerId: user?.stripeCustomerId || '',
-  });
-
-  res.status(204).json(result);
-};
-
-export const getUsersCardByIdValidation = {
-  params: Joi.object({ id: Joi.string().required() }),
-};
-export const getUsersCardById = () => async (req: Request, res: Response): Promise<void> => {
-  const {
-    user,
-    params: { id },
-  } = req;
-
-  const service = new StripeCardsService();
-  const result = await service.execute({
-    stripeCustomerId: user?.stripeCustomerId || '',
-    stripeCardId: id,
-    action: Actions.READ,
   });
 
   res.status(200).json(result);
