83fd7ee31834cc70686da0531bc431e06c19175e
dhairyashah <dshah4@albany.edu>
2023-04-15 04:59:48 -0400

    Added send Mail API for become Seller

--- src/database/seed/htmlTemplates/register.ts
+++ src/database/seed/htmlTemplates/register.ts
@@ -211,3 +211,78 @@ export const orderSuccess = `<body style="margin: 0px; padding: 0px; background-
     </tbody>
 </table>
 </body>`;
+
+export const sellerMessage = `
+<html><head></head><body style="margin: 0px; padding: 0px; background-color: #f9f9f9;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f9f9f9">
+    <tbody>
+        <tr>
+            <td align="top">
+                <div align="center">
+                    <table width="620" style="margin: 30px 0px;" cellspacing="0" cellpadding="0" bgcolor="#fff">
+                        <tbody>
+                            
+                            <tr>
+                                <td style="padding-top: 25px;">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-size:14px; color: #263b5f; font-family: Verdana, Arial,Helvetica,sans-serif; line-height: 1.5;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding: 0px 30px 10px 30px;" colspan="3">
+                                                    <span style="font-size: 18px; display: block;">Hi {name},</span>
+                                                    <p style="margin-top: 5px;">Welcome to WebMart. </p>
+                                                    <p style="margin-bottom: 0px;">{message} </p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-size:14px; color: #263b5f; font-family: Verdana, Arial,Helvetica,sans-serif; line-height: 1.5;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding: 0px 30px;">
+                                                    <p style="margin-top: 0px; margin-bottom: 20px;"> We hope you love our service as much as we enjoy delivering to you.</p>
+                                                    
+                                                    
+                                                </td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                <td style="padding: 0px 30px;">
+                                                    Thank you, <br> WebMart                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-size: 12px; font-style: italic; color: #6c757d; padding: 20px 30px; border-radius: 0px 0px 4px 4px;">
+                                                    <strong>Note:</strong> The content of this email is intended only for use by the individual or entity to whom it is addressed.
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-size:14px; color: #263b5f; font-family: Verdana, Arial,Helvetica,sans-serif; line-height: 1.5;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding: 0px 30px; vertical-align: top; background: #f9f9f9; color: #898f95; text-align: center;"><p style="font-size: 14px; padding-top: 15px; margin-bottom: 8px; margin-top: 0px; border-top: 1px solid #ecf4fd;">© Copyright 2023 by WebMart. All Rights Reserved.</p>
+                                                    <small>Please do not reply to this message; it was sent from an unmonitored email address.</small> <br>
+                                                    <small>This message is a service email related to your use of WebMart.</small>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</body></html>
+`;
