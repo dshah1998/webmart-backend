5c1dd0be3b7e8cfe29c8fb26716036ac4c18ce29
sarangp3010 <spatel25@albany.edu>
2023-04-16 17:53:05 -0400

    request controller

---
+++ src/controller/modificationRequests.ts
@@ -0,0 +1,107 @@
+import {
+  getManager,
+  getRepository,
+  getCustomRepository,
+  FindConditions,
+  Like,
+  Not,
+} from "typeorm";
+import { Request, Response } from "express";
+import { Joi } from "express-validation";
+
+import { BadRequestError } from "../error";
+import { ModificationRequestsRepository } from "../repository/ModificationRequests";
+
+import { Products } from "../model/Products";
+import { ModificationRequests } from "../model/ModificationRequests";
+
+export const getAll =
+  () =>
+  async (req: Request, res: Response): Promise<void> => {
+    const { user } = req;
+    const [notifications, count] = await getManager()
+      .createQueryBuilder(ModificationRequests, "requests")
+      .leftJoinAndSelect("requests.user", "user")
+      .where("user.id = :userId", { userId: user?.id })
+      .leftJoinAndSelect("requests.product", "product")
+      .getManyAndCount();
+
+    res.status(200).json({ count, notifications });
+  };
+
+export const createNotificationValidation = {
+  body: Joi.object({
+    comments: Joi.string().max(255).required(),
+    productId: Joi.string().uuid({ version: "uuidv4" }).required(),
+    status: Joi.string().default("pending")
+  }),
+};
+export const createNotification =
+  () =>
+  async (req: Request, res: Response): Promise<void> => {
+    const {
+      user,
+      body: { comments, productId, status },
+    } = req;
+
+    const requestRepository = getRepository(ModificationRequests);
+    const existingrequest = await requestRepository.findOne({
+      where: { comments, status },
+    });
+
+    if (existingrequest) {
+      throw new BadRequestError(
+        "Request is already added",
+        "Request_ALREADY_EXIST"
+      );
+    }
+
+    let newRequest = requestRepository.create({
+        product: productId && (await getManager().getRepository(Products).findOneOrFail(productId)),
+        user,
+        comments,
+        status
+      });
+
+      newRequest = await requestRepository.save(newRequest);
+
+    res.status(201).json(newRequest);
+  };
+
+export const updateRequestValidation = {
+  body: Joi.object({
+    status: Joi.string().default("pending").required()
+  }),
+};
+export const updateRequest =
+  () =>
+  async (req: Request, res: Response): Promise<void> => {
+    const {
+      body: { status },
+      params: { id },
+    } = req;
+
+    const notificationRepository = getCustomRepository(ModificationRequestsRepository);
+    await notificationRepository.update(id, {
+        status
+    });
+
+    res.sendStatus(204);
+  };
+
+export const deleteremoveRequestValidation = {
+  params: Joi.object({ id: Joi.number().required() }),
+};
+export const removeRequest =
+  () =>
+  async (req: Request, res: Response): Promise<void> => {
+    const {
+      params: { id },
+    } = req;
+
+    await getManager().transaction(async (em) => {
+      await em.delete(ModificationRequestsRepository, id);
+    });
+
+    res.sendStatus(204);
+  };
--- src/route/modificationRequests.ts
+++ src/route/modificationRequests.ts
@@ -1,71 +1,57 @@
-// import { Router } from 'express';
-// import { validate } from 'express-validation';
+import { Router } from 'express';
+import { validate } from 'express-validation';
 
-// import { handleError, authenticate, checkUserType } from '../middleware';
-// import {
-//   createModifyRequestValidation,
-//   createModifyRequest,
-//   getModifyRequestValidation,
-//   getAll,
-//   deleteModifyRequestValidation,
-//   removeModifyRequest,
-//   updateModifyRequestById,
-//   updateModifyRequestByIdValidation,
-//   getModifyRequestById,
-//   getModifyRequestByIdValidation
-// } from '../controller/carts';
+import { handleError, authenticate, checkUserType } from '../middleware';
+import {
+  getAll,
+  createNotificationValidation,
+  createNotification,
+  updateRequestValidation,
+  updateRequest,
+  deleteremoveRequestValidation,
+  removeRequest
+} from '../controller/modificationRequests';
 
-// import { WebMartUserType } from '../constants';
+import { WebMartUserType } from '../constants';
 
-// const router = Router();
+const router = Router();
 
-// const getModifyRequests = (): Router => 
-//   router.get(
-//     '/',
-//     authenticate,
-//     validate(getModifyRequestValidation, { context: true }),
-//     handleError(getAll()),
-//   );
+const getModifyRequests = (): Router => 
+  router.get(
+    '/',
+    authenticate,
+    handleError(getAll()),
+  );
 
-// const postCreateModifyRequest = (): Router =>
-//   router.post(
-//     '/',
-//     authenticate,
-//     checkUserType(WebMartUserType.ADMIN, WebMartUserType.USER),
-//     validate(createModifyRequestValidation, { context: true }),
-//     handleError(createModifyRequest()),
-//   );
+const postCreateModifyRequest = (): Router =>
+  router.post(
+    '/',
+    authenticate,
+    validate(createNotificationValidation, { context: true }),
+    handleError(createNotification()),
+  );
 
-// const getModifyRequestUsingId = (): Router =>
-// router.get(
-//   '/:id',
-//   authenticate,
-//   validate(getModifyRequestByIdValidation, { context: true }),
-//   handleError(getModifyRequestById()),
-// );
+const deleteUpdateModifyRequest = (): Router =>
+  router.delete(
+    '/:id',
+    authenticate,
+    checkUserType(WebMartUserType.ADMIN, WebMartUserType.USER, WebMartUserType.SELLER),
+    validate(deleteremoveRequestValidation, { context: true }),
+    handleError(removeRequest()),
+  );
 
-// const deleteUpdateModifyRequest = (): Router =>
-//   router.delete(
-//     '/:id',
-//     authenticate,
-//     checkUserType(WebMartUserType.ADMIN, WebMartUserType.USER, WebMartUserType.SELLER),
-//     validate(deleteModifyRequestValidation, { context: true }),
-//     handleError(removeModifyRequest()),
-//   );
+  const updateModifyRequests = (): Router =>
+  router.put(
+    '/:id',
+    authenticate,
+    validate(updateRequestValidation, { context: true }),
+    handleError(updateRequest()),
+  );
 
-//   const updateModifyRequests = (): Router =>
-//   router.put(
-//     '/:id',
-//     authenticate,
-//     validate(updateModifyRequestByIdValidation, { context: true }),
-//     handleError(updateModifyRequestById()),
-//   );
-
-// export default (): Router =>
-//   router.use([
-//     getModifyRequests(),
-//     postCreateModifyRequest(),
-//     getModifyRequestUsingId(),
-//     deleteUpdateModifyRequest(),
-//     updateModifyRequests(),
-//   ]);
+export default (): Router =>
+  router.use([
+    getModifyRequests(),
+    postCreateModifyRequest(),
+    deleteUpdateModifyRequest(),
+    updateModifyRequests(),
+  ]);
